d93eab2e48c2910d9791b352a0db669b23194345
elic2real <elic2real@gmail.com>
2025-06-19 19:50:07 -0600

    Create MainActivity.java
    Add missing main activity Java

---
+++ android/app/src/main/java/com/aim/metadesign/MainActivity.java
@@ -0,0 +1,10 @@
+package com.aim.metadesign;
+
+// Import the Capacitor BridgeActivity, which provides the integration between Android and your web app
+import com.getcapacitor.BridgeActivity;
+
+// MainActivity is the entry point of your Android app
+public class MainActivity extends BridgeActivity {
+    // If you need to customize native behavior (register plugins, override lifecycle methods, etc.)
+    // you can add code here. For most Capacitor apps, this empty class is sufficient.
+}
